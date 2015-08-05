b423f636c8d3c8510de043d6dcd68c58a3cd0609
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-08-05 13:42:01 -0400

    Update Differential_Expression.R

--- Differential_Expression.R
+++ Differential_Expression.R
@@ -2,18 +2,19 @@
 # dat is the input expression matrix
 # pData is the sample information
 # cont is for contrasts
-# res.case.ctrl <- diffexp.func(expr = '~0+treat+age+gender+site', dat = voom.data.expr, pData = pData, cont='treatDisease-treatNormal')
+# tmp <- diffexp.func(expr = '~0+treat+age+gender+site', dat = voom.data.expr, pData = pData, cont1 = 'treatDisease', cont2 = 'treatNormal')
 
-diffexp.func <- function(expr,dat,pData,cont)
+diffexp.func <- function(expr,dat,pData,cont1,cont2)
 {
+  cont <- paste(cont1,"-",cont2, sep="")
   expr <- eval(parse(text=expr))
   design = model.matrix(expr, data = pData)
   fit = lmFit(dat, design = design)
-  contrast.matrix = makeContrasts(cont, levels = design)
+  contrast.matrix = makeContrasts(contrasts = cont, levels = design)
   fit2 = contrasts.fit(fit, contrast.matrix)
   fit2 = eBayes(fit2)
   res = toptable(fit = fit2, number = Inf, adjust.method = "BH",p.value = 1) 
   res$FC = 2^(abs(res$logFC))
   res$FC = ifelse(res$logFC<0,res$FC*(-1),res$FC*1)
-  return(res.casevsctrl.novar)
+  return(res)
 }

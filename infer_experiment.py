17e306796c553bbc775a6f96e1bc69892f5e5f6c
Komal Rathi <komalsrathi@users.noreply.github.com>
2015-09-03 15:51:33 -0400

    Infer Paired End Experiment for RNASeq

---
+++ infer_experiment.py
@@ -0,0 +1,5 @@
+# available at RSeQC http://rseqc.sourceforge.net/ 
+./infer_experiment.py -i sample.bam -r hg19_Ensembl.bed
+
+# hg19_Ensembl.bed is the gene model and can be downloaded from 
+wget http://dldcc-web.brc.bcm.edu/lilab/liguow/RSeQC/dat/hg19_Ensembl.bed.gz
